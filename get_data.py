993b81bc5f82ffcd8eb9c8e25c2ced4f95ca5182
rosdyana <rosdyana.kusuma@gmail.com>
2017-12-07 10:34:58 +0800

    add get data script and preprocessing script

---
+++ get_data.py
@@ -0,0 +1,85 @@
+# Alpha Vantage API Key: 4BWQGJ75VET4DK8I
+# Document: https://www.alphavantage.co/documentation/
+# https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=1min&apikey=
+# https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=MSFT&outputsize=full&apikey=4BWQGJ75VET4DK8I
+
+# Tiingo
+# https://api.tiingo.com/docs/tiingo/daily#priceData
+# https://api.tiingo.com/services/tiingo/daily
+# Auth Token: bf4f408337a25e48b67fdc365ae25476c3fba483
+# https://api.tiingo.com/tiingo/daily/googl/prices?startDate=2016-1-1&endDate=2017-1-1&token=bf4f408337a25e48b67fdc365ae25476c3fba48
+# pd.read_json("https://api.tiingo.com/tiingo/daily/googl/prices?startDate=2016-1-1&endDate=2017-1-1&token=bf4f408337a25e48b67fdc365ae25476c3fba483")
+
+# https://stooq.com
+# Where should I get API?
+
+# http://quant.caltech.edu/historical-stock-data.html
+
+
+import argparse
+import arrow
+import pandas as pd
+import datetime as dt
+from pandas_datareader import data, wb
+import os
+import fix_yahoo_finance as yf
+import time
+# fixed pandas_datareader can't download from yahoo finance
+yf.pdr_override()
+
+def main():
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-sd', '--start_date', type=str,
+                        default='1990-01-01', help='Start date parameter value - format YYYY-MM-DD')
+    parser.add_argument('-ed', '--end_date', type=str,
+                        default=arrow.now().format('YYYY-MM-DD'), help='End date parameter - format YYYY-MM-DD')
+    parser.add_argument('-t', '--ticker', nargs='+', help='<Required> Set flag', required=True)
+    parser.add_argument('-s', '--source', help='<Required> set source', required=True)
+    parser.add_argument('-a', '--attempt', help='set max attempt to download', default=10)
+    parser.add_argument('-e', '--exist', help='check exist stock history file', default=False)
+    args = parser.parse_args()
+    # # fetch all data
+
+
+
+
+    if args.source == "tiingo":
+        for ticker in set(args.ticker):
+            fetch_tiingo_data(ticker, args.start_date, args.end_date, "stockdatas/{}.csv".format(ticker))
+    elif args.source == "yahoo":
+        for ticker in set(args.ticker):
+            fetch_yahoo_data(ticker, args.start_date, args.end_date, "stockdatas/{}.csv".format(ticker), args.attempt, args.exist)
+
+
+def fetch_tiingo_data(ticker, start_date, end_date, fname):
+    url = "https://api.tiingo.com/tiingo/daily/{ticker}/prices?startDate={start_date}&endDate={end_date}&token={token}"
+    token = "ca5a6f47a99ae61051e4de63b26f727b1709a01d"
+    data = pd.read_json(url.format(
+            ticker=ticker,
+            start_date=start_date,
+            end_date=end_date,
+            token=token
+    ))
+    data.to_csv(fname, columns=["date","open","close","high","low","volume","adjClose"], index=False)
+
+def fetch_yahoo_data(ticker, start_date, end_date, fname, max_attempt, check_exist):
+    if (os.path.exists(fname) == True) and check_exist:
+        print("file exist")
+    else:
+        for attempt in range(max_attempt):
+            time.sleep(2)
+            try:
+                dat = data.DataReader(''.join("{}".format(
+                    ticker)), 'yahoo', start_date, end_date)
+                dat.to_csv(fname)
+            except Exception as e:
+                if attempt < max_attempt - 1:
+                    print('Attempt {}: {}'.format(attempt + 1, str(e)))
+                else:
+                    raise
+            else:
+                break
+
+if __name__ == '__main__':
+    main()
--- preprocess.py
+++ preprocess.py
@@ -1,20 +1,157 @@
 import pandas as pd
+import plotly.offline as offline
+import matplotlib.pyplot as plt
+from plotly.tools import FigureFactory as FF
+from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
+# offline.init_notebook_mode()
 
-# read stock data
-df = pd.read_csv('SPY.csv', header=None, index_col=0)
-# drop date and volume columns
-df.drop(df.columns[[4, 5]], axis=1, inplace=True)
-df = df.astype(str)
-separators = pd.DataFrame(', ', df.index, df.columns[:-1])
-separators[df.columns[-1]] = '\n'
-# print (df + separators).sum(axis=1).sum()
-data = df[1:]
-# print(data.head())
+import argparse
 
-for i in range(0, len(data), 20):
+import decimal
 
-    c = data[i:i + 20]
-    print(c)
-    print("\nnext\n")
-# resize 224x224 imagemagick
-# find . -maxdepth 1 -iname "*.png" | xargs -L1 -I{} convert -adaptive-resize 224x224! "{}" "{}"
+def drange(x, y, jump):
+  while x < y:
+    yield float(x)
+    x += decimal.Decimal(jump)
+
+def isnan(value):
+    try:
+        import math
+        return math.isnan(float(value))
+    except:
+        return False
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-i', '--input',
+                        help='an csv file of stock data', required=True)
+    parser.add_argument('-l', '--seq_len',
+                        help='num of sequence length', default=20)
+    parser.add_argument('-m', '--mode',
+                        help='mode of preprocessing data', required=True)
+    args = parser.parse_args()
+    if args.mode == 'convert2image':
+        convert2image(args.input,args.seq_len)
+    if args.mode == 'createLabel':
+        createLabel(args.input, args.seq_len)
+    if args.mode == 'img2dt':
+        image2dataset(args.input)
+
+def image2dataset(input):
+    img = load_img(input)  # this is a PIL image
+    x = img_to_array(img)  # this is a Numpy array with shape (3, 150, 150)
+    print(x.shape)
+    x = x.reshape((1,) + x.shape)  # this is a Numpy array with shape (1, 3, 150, 150)
+    #print(x)
+
+def createLabel(fname, seq_len):
+    # import plotly.graph_objs as go
+    #py.sign_in('rosdyana', 'eVtlDykeB8gMHmp6y4Ff')
+    # read stock data
+    df = pd.read_csv(fname, header=None, index_col=0)
+    df = df[4]
+    df.fillna(0)
+    # df = df.astype(str)
+    # separators = pd.DataFrame(', ', df.index, df.columns[:-1])
+    # separators[df.columns[-1]] = '\n'
+    # print (df + separators).sum(axis=1).sum()
+
+    data = df[1:]
+    # print(data)
+
+    for i in range(0, len(data), int(seq_len)):
+        #print("idx : {}".format(i))
+        c = data[i:i + int(seq_len)]
+        # print(len(c))
+        starting = 0
+        endvalue = 0
+        label = ""
+        for idx, val in enumerate(c):
+            if idx == 0:
+                starting = float(val)
+            if idx == len(c) - 1:
+                endvalue = float(val)
+        sizeincrease = endvalue - starting
+        diff = sizeincrease / starting
+        perct = diff * 100
+        if isnan(perct):
+            perct = 0
+        if perct < -1.5:
+            label = "A"
+        if perct > -1.5 and perct < -0.5:
+        #if perct in range(-1.5, -0.5):
+            label = "B"
+        if perct > -0.5 and perct < 0.4:
+        #if perct in range(-0.5, 0.4):
+            label = "C"
+        if perct > 0.4 and perct < 1.4:
+        #if perct in range(0.4, 1.4):
+            label = "D"
+        if perct > 1.4 and perct < 2.5:
+        #if perct in range(1.4, 2.5):
+            label = "E"
+        if perct > 2.5 and perct < 4.3:
+        #if perct in range(2.5, 4.3):
+            label = "F"
+        if perct > 4.3:
+            label = "G"
+        # print("{},{}-{}".format(perct, fname[12:-4], i))
+        with open("{}_label.txt".format(fname[12:-4]), 'a') as the_file:
+            the_file.write("{},{}-{}\n".format(label, fname[12:-4], i))
+
+
+def convert2image(fname, seq_len):
+    # import plotly.graph_objs as go
+    #py.sign_in('rosdyana', 'eVtlDykeB8gMHmp6y4Ff')
+    # read stock data
+    df = pd.read_csv(fname, header=None, index_col=0)
+    df.fillna(0)
+    # drop date and volume columns
+    df.drop(df.columns[[4, 5]], axis=1, inplace=True)
+    df = df.astype(str)
+    separators = pd.DataFrame(', ', df.index, df.columns[:-1])
+    separators[df.columns[-1]] = '\n'
+    # print (df + separators).sum(axis=1).sum()
+    data = df[1:]
+    # print(data.head())
+
+    for i in range(0, len(data), int(seq_len)):
+        print("idx : {}".format(i))
+        c = data[i:i + int(seq_len)]
+        fig = FF.create_candlestick(
+            open=c[1], high=c[2], low=c[3], close=c[4])
+
+        fig['layout'].update({
+            'xaxis': dict(visible=False),
+            'yaxis': dict(visible=False),
+            'paper_bgcolor': 'rgba(1,1,1,1)',
+            'plot_bgcolor': 'rgba(1,1,1,1)'
+        })
+        #plot_mpl(fig, image='png')
+        #py.image.save_as(fig, filename='dataset/images/{}.png'.format(i))
+        offline.plot(fig, filename='dataset/images/{}-{}.html'.format(fname[12:-4], i),
+                     image='png', auto_open=False, show_link=False, image_filename='dataset/images/{}-{}.png'.format(fname[11:-4], i))
+    # find . -maxdepth 1 -iname "*.png" | xargs -L1 -I{} convert -adaptive-resize 48x48! "{}" "{}"
+    # import time
+    # from selenium import webdriver
+    #
+    # profile = webdriver.FirefoxProfile()
+    # profile.set_preference('browser.download.folderList', 2)  # custom location
+    # profile.set_preference('browser.download.manager.showWhenStarting', False)
+    # profile.set_preference('browser.download.dir', '/tmp')
+    # profile.set_preference('browser.helperApps.neverAsk.saveToDisk', 'image/png')
+    #
+    # driver = webdriver.Firefox(firefox_profile=profile)
+    # import glob, os
+    # os.chdir("dataset/images")
+    # for file in glob.glob("*.html"):
+    #     #file:///home/ros/ROS/research/Deep_Stock_Representation_learning%20-%20from_candlestick_charts_to_investment_decisions/dataset/images/4100.html
+    #     driver.get("file:///home/ros/ROS/research/Deep_Stock_Representation_learning%20-%20from_candlestick_charts_to_investment_decisions/dataset/images/{}".format(file))
+    #     export_button = driver.find_element_by_xpath("//a[@data-title='Download plot as a png']")
+    #     export_button.click()
+    #     time.sleep(10)
+    #     driver.quit()
+if __name__ == '__main__':
+    main()
