7b337d20ef63a4a2bcdafb5b7c636cd27c983ecd
rosdyana <rosdyana.kusuma@gmail.com>
2017-12-07 15:28:07 +0800

    fix model

--- resnet50.py
+++ resnet50.py
@@ -2,48 +2,116 @@ import math, json, os, sys
 
 import keras
 from keras.callbacks import EarlyStopping, ModelCheckpoint
-from keras.layers import Dense
 from keras.models import Model
 from keras.optimizers import Adam
 from keras.preprocessing import image
+from keras.applications.resnet50 import conv_block, identity_block
+from keras.layers import (Activation, BatchNormalization, Convolution2D, Dense,
+                          Flatten, Input, MaxPooling2D, ZeroPadding2D, AveragePooling2D)
+from keras import backend as K
+import numpy as np
+from keras import applications
+from keras.preprocessing.image import ImageDataGenerator
 
 
-DATA_DIR = 'data'
-TRAIN_DIR = os.path.join(DATA_DIR, 'training')
-VALID_DIR = os.path.join(DATA_DIR, 'validation')
-SIZE = (48, 48)
-BATCH_SIZE = 16
+# dimensions of our images.
+img_width, img_height = 48, 48
+channel = 3
+top_model_weights_path = 'bottleneck_fc_model.h5'
+train_data_dir = 'data/training'
+validation_data_dir = 'data/validation'
+nb_train_samples = 272
+nb_validation_samples = 80
+epochs = 100
+batch_size = 16
+SHAPE = (img_width, img_height ,channel)
+bn_axis = 3 if K.image_dim_ordering() == 'tf' else 1
 
+def save_bottlebeck_features():
+    datagen = ImageDataGenerator(rescale=1./255)
+
+    # build the VGG16 network
+    model = applications.ResNet50(include_top=False, weights='imagenet')
+
+    generator = datagen.flow_from_directory(
+        train_data_dir,
+        target_size=(img_width, img_height),
+        batch_size=batch_size,
+        class_mode=None,
+        shuffle=False)
+    bottleneck_features_train = model.predict_generator(
+        generator, nb_train_samples // batch_size)
+    np.save('bottleneck_features_train_resnet50',bottleneck_features_train)
+
+    generator = datagen.flow_from_directory(
+        validation_data_dir,
+        target_size=(img_width, img_height),
+        batch_size=batch_size,
+        class_mode=None,
+        shuffle=False)
+    bottleneck_features_validation = model.predict_generator(
+        generator, nb_validation_samples // batch_size)
+    np.save('bottleneck_features_validation_resnet50',bottleneck_features_validation)
+
+def build_model(seed=None):
+    # We can't use ResNet50 directly, as it might cause a negative dimension
+    # error.
+    if seed:
+        np.random.seed(seed)
+
+    input_layer = Input(shape=SHAPE)
+
+    x = ZeroPadding2D((3, 3))(input_layer)
+    x = Convolution2D(64, 7, 7, subsample=(2, 2), name='conv1')(x)
+    x = BatchNormalization(axis=bn_axis, name='bn_conv1')(x)
+    x = Activation('relu')(x)
+    x = MaxPooling2D((3, 3), strides=(2, 2))(x)
+
+    x = conv_block(x, 3, [64, 64, 256], stage=2, block='a', strides=(1, 1))
+    x = identity_block(x, 3, [64, 64, 256], stage=2, block='b')
+    x = identity_block(x, 3, [64, 64, 256], stage=2, block='c')
+
+    x = conv_block(x, 3, [128, 128, 512], stage=3, block='a')
+    x = identity_block(x, 3, [128, 128, 512], stage=3, block='b')
+    x = identity_block(x, 3, [128, 128, 512], stage=3, block='c')
+    x = identity_block(x, 3, [128, 128, 512], stage=3, block='d')
+
+    x = conv_block(x, 3, [256, 256, 1024], stage=4, block='a')
+    x = identity_block(x, 3, [256, 256, 1024], stage=4, block='b')
+    x = identity_block(x, 3, [256, 256, 1024], stage=4, block='c')
+    x = identity_block(x, 3, [256, 256, 1024], stage=4, block='d')
+    x = identity_block(x, 3, [256, 256, 1024], stage=4, block='e')
+    x = identity_block(x, 3, [256, 256, 1024], stage=4, block='f')
+
+
+    x = conv_block(x, 3, [512, 512, 2048], stage=5, block='a')
+    x = identity_block(x, 3, [512, 512, 2048], stage=5, block='b')
+    x = identity_block(x, 3, [512, 512, 2048], stage=5, block='c')
+    #print(x)
+    #x = AveragePooling2D((7, 7), name='avg_pool')(x)
+
+
+    x = Flatten()(x)
+    x = Dense(2, activation='softmax', name='fc10')(x)
+
+    model = Model(input_layer, x)
+
+    return model
 
 if __name__ == "__main__":
-    num_train_samples = sum([len(files) for r, d, files in os.walk(TRAIN_DIR)])
-    num_valid_samples = sum([len(files) for r, d, files in os.walk(VALID_DIR)])
+    save_bottlebeck_features()
+    train_data = np.load('bottleneck_features_train_resnet50.npy')
+    train_labels = np.array(
+        [0] * (nb_train_samples // 2) + [1] * (nb_train_samples // 2))
 
-    num_train_steps = math.floor(num_train_samples/BATCH_SIZE)
-    num_valid_steps = math.floor(num_valid_samples/BATCH_SIZE)
+    validation_data = np.load('bottleneck_features_validation_resnet50.npy')
+    validation_labels = np.array(
+        [0] * (nb_validation_samples // 2) + [1] * (nb_validation_samples // 2))
 
-    gen = keras.preprocessing.image.ImageDataGenerator()
-    val_gen = keras.preprocessing.image.ImageDataGenerator(horizontal_flip=True, vertical_flip=True)
-
-    batches = gen.flow_from_directory(TRAIN_DIR, target_size=SIZE, class_mode='categorical', shuffle=True, batch_size=BATCH_SIZE)
-    val_batches = val_gen.flow_from_directory(VALID_DIR, target_size=SIZE, class_mode='categorical', shuffle=True, batch_size=BATCH_SIZE)
-
-    model = keras.applications.resnet50.ResNet50()
-
-    classes = list(iter(batches.class_indices))
-    model.layers.pop()
-    for layer in model.layers:
-        layer.trainable=False
-    last = model.layers[-1].output
-    x = Dense(len(classes), activation="softmax")(last)
-    finetuned_model = Model(model.input, x)
-    finetuned_model.compile(optimizer=Adam(lr=0.0001), loss='categorical_crossentropy', metrics=['accuracy'])
-    for c in batches.class_indices:
-        classes[batches.class_indices[c]] = c
-    finetuned_model.classes = classes
-
-    early_stopping = EarlyStopping(patience=10)
-    checkpointer = ModelCheckpoint('resnet50_best.h5', verbose=1, save_best_only=True)
-
-    finetuned_model.fit_generator(batches, steps_per_epoch=num_train_steps, epochs=1000, callbacks=[early_stopping, checkpointer], validation_data=val_batches, validation_steps=num_valid_steps)
-finetuned_model.save('resnet50_final.h5')
+    model = build_model()
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+    model.fit(train_data, train_labels, batch_size=64, nb_epoch=60)
+    model.save('resnet50_final.h5')
+    score = model.evaluate(validation_data=val_batches, verbose=1)
+    print('\n\nOverall Test score:', score[0])
+    print('Overall Test accuracy:', score[1])
--- vgg16.py
+++ vgg16.py
@@ -1,40 +1,3 @@
-'''This script goes along the blog post
-"Building powerful image classification models using very little data"
-from blog.keras.io.
-It uses data that can be downloaded at:
-https://www.kaggle.com/c/dogs-vs-cats/data
-In our setup, we:
-- created a data/ folder
-- created train/ and validation/ subfolders inside data/
-- created cats/ and dogs/ subfolders inside train/ and validation/
-- put the cat pictures index 0-999 in data/train/cats
-- put the cat pictures index 1000-1400 in data/validation/cats
-- put the dogs pictures index 12500-13499 in data/train/dogs
-- put the dog pictures index 13500-13900 in data/validation/dogs
-So that we have 1000 training examples for each class, and 400 validation examples for each class.
-In summary, this is our directory structure:
-```
-data/
-    train/
-        dogs/
-            dog001.jpg
-            dog002.jpg
-            ...
-        cats/
-            cat001.jpg
-            cat002.jpg
-            ...
-    validation/
-        dogs/
-            dog001.jpg
-            dog002.jpg
-            ...
-        cats/
-            cat001.jpg
-            cat002.jpg
-            ...
-```
-'''
 import numpy as np
 from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
@@ -49,7 +12,7 @@ train_data_dir = 'data/training'
 validation_data_dir = 'data/validation'
 nb_train_samples = 272
 nb_validation_samples = 80
-epochs = 50
+epochs = 100
 batch_size = 16
 
 
@@ -102,6 +65,10 @@ def train_top_model():
               epochs=epochs,
               batch_size=batch_size,
               validation_data=(validation_data, validation_labels))
+
+    score = model.evaluate(validation_data, validation_labels, verbose=1)
+    print('\n\nOverall Test score:', score[0])
+    print('Overall Test accuracy:', score[1])
     model.save_weights(top_model_weights_path)
 
 
