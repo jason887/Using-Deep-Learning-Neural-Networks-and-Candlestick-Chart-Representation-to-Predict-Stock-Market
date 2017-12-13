e0b508e9dd2a1913d05f1307aac37fd232949cde
rosdyana <rosdyana.kusuma@gmail.com>
2017-12-13 10:30:07 +0800

    remove unused files

--- experiment/coba.py
+++
@@ -1,5 +0,0 @@
-blocks = [2, 2, 2, 2]
-for stage_id, iterations in enumerate(blocks):
-    print("iterations : {}".format(iterations))
-    for block_id in range(iterations):
-        print("stage_id : {}".format(stage_id))
