448e7e1dace4f06799c0eef559e3a32660152227
rosdyana <rosdyana.kusuma@gmail.com>
2017-12-14 12:26:55 +0800

    add resnet2

---
+++ resnet2.py
@@ -0,0 +1,126 @@
+# import tensorflow as tf # uncomment this for using GPU
+import os
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"   # comment this for using GPU
+os.environ["CUDA_VISIBLE_DEVICES"] = ""  # change with 1 for using GPU
+# uncomment below for using GPU
+# config = tf.ConfigProto()
+# # maximun alloc gpu50% of MEM
+# config.gpu_options.per_process_gpu_memory_fraction = 0.5
+# #allocate dynamically
+# config.gpu_options.allow_growth = True
+# sess = tf.Session(config = config)
+
+import math, json, os, sys
+
+import keras
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+from keras.models import Model
+from keras.optimizers import Adam
+from keras.preprocessing import image
+from keras.applications.resnet50 import conv_block, identity_block
+from keras.layers import (Activation, BatchNormalization, Convolution2D, Dense,
+                          Flatten, Input, MaxPooling2D, ZeroPadding2D, AveragePooling2D)
+from keras import backend as K
+import numpy as np
+from keras import applications
+from keras.preprocessing.image import ImageDataGenerator
+from keras.utils import np_utils
+import dataset
+import argparse
+
+import time
+from datetime import timedelta
+
+def build_dataset(data_directory, img_width):
+    X, y, tags = dataset.dataset(data_directory, int(img_width))
+    nb_classes = len(tags)
+
+    sample_count = len(y)
+    train_size = sample_count * 4 // 5
+    print("train size : {}".format(train_size))
+    X_train = X[:train_size]
+    y_train = y[:train_size]
+    Y_train = np_utils.to_categorical(y_train, nb_classes)
+    X_test  = X[train_size:]
+    y_test  = y[train_size:]
+    Y_test = np_utils.to_categorical(y_test, nb_classes)
+    return X_train, Y_train, X_test, Y_test, nb_classes
+
+def build_model(SHAPE,nb_classes,bn_axis,seed=None):
+    # We can't use ResNet50 directly, as it might cause a negative dimension
+    # error.
+    if seed:
+        np.random.seed(seed)
+
+    input_layer = Input(shape=SHAPE)
+
+    x = ZeroPadding2D((3, 3))(input_layer)
+    x = Convolution2D(64, 7, 7, subsample=(2, 2), name='conv1')(x)
+    x = BatchNormalization(axis=bn_axis, name='bn_conv1')(x)
+    x = Activation('relu')(x)
+    x = MaxPooling2D((3, 3), strides=(2, 2))(x)
+
+    x = conv_block(x, 3, [64, 64, 256], stage=2, block='a', strides=(1, 1))
+    x = identity_block(x, 3, [64, 64, 256], stage=2, block='b')
+    x = identity_block(x, 3, [64, 64, 256], stage=2, block='c')
+
+    #print(x)
+    #x = AveragePooling2D((7, 7), name='avg_pool')(x)
+
+
+    x = Flatten()(x)
+    x = Dense(nb_classes, activation='softmax', name='fc10')(x)
+
+    model = Model(input_layer, x)
+
+    return model
+
+def main():
+    start_time = time.monotonic()
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-i', '--input',
+                        help='an input directory of dataset', required=True)
+    parser.add_argument('-d', '--dimension',
+                        help='a image dimension', type=int, default=200)
+    parser.add_argument('-c', '--channel',
+                        help='a image channel', type=int, default=3)
+    parser.add_argument('-e', '--epochs',
+                        help='num of epochs', type=int, default=10)
+    parser.add_argument('-b', '--batch_size',
+                        help='num of batch_size', type=int, default=64)
+    parser.add_argument('-o', '--optimizer',
+                        help='choose the optimizer (rmsprop, adagrad, adadelta, adam, adamax, nadam)', default="adam")
+    args = parser.parse_args()
+    # dimensions of our images.
+    img_width, img_height = args.dimension, args.dimension
+    channel = args.channel
+    epochs = args.epochs
+    batch_size = args.batch_size
+    SHAPE = (img_width, img_height ,channel)
+    # print("SHAPE : {}".format(SHAPE))
+    bn_axis = 3 if K.image_dim_ordering() == 'tf' else 1
+
+    data_directory = args.input
+    period_name = data_directory.split('/')
+
+    print ("loading dataset")
+    X_train, Y_train, X_test, Y_test, nb_classes= build_dataset(data_directory, args.dimension)
+    print("number of classes : {}".format(nb_classes))
+
+    model = build_model(SHAPE,nb_classes,bn_axis)
+
+    model.compile(optimizer=args.optimizer, loss='categorical_crossentropy', metrics=['accuracy'])
+
+    # Fit the model
+    model.fit(X_train, Y_train, batch_size=batch_size, epochs=epochs)
+
+    # Save Model or creates a HDF5 file
+    model.save('{}epochs_{}period_{}dimension_resnet2_model.h5'.format(epochs,period_name[2],period_name[1]), overwrite=True)
+    #del model  # deletes the existing model
+
+    end_time = time.monotonic()
+    print("Duration : {}".format(timedelta(seconds=end_time - start_time)))
+
+if __name__ == "__main__":
+    main()
