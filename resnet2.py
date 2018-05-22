b4bfc1c66011147ef63cb4c4134131e9b30dea3b
Rosdyana Kusuma <rosdyana.kusuma@gmail.com>
2018-05-22 12:20:21 +0800

    modify resnet2

--- resnet2.py
+++ resnet2.py
@@ -1,52 +1,130 @@
-# import tensorflow as tf # uncomment this for using GPU
+import tensorflow as tf
 import os
-os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"   # comment this for using GPU
-os.environ["CUDA_VISIBLE_DEVICES"] = ""  # change with 1 for using GPU
-# uncomment below for using GPU
-# config = tf.ConfigProto()
-# # maximun alloc gpu50% of MEM
-# config.gpu_options.per_process_gpu_memory_fraction = 0.5
-# #allocate dynamically
-# config.gpu_options.allow_growth = True
-# sess = tf.Session(config = config)
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True
+sess = tf.Session(config=config)
 
-import math, json, os, sys
+
+import math
+import json
+import sys
 
 import keras
-from keras.callbacks import EarlyStopping, ModelCheckpoint
+from keras.layers import Input, Dense, Conv2D, MaxPooling2D, AveragePooling2D, ZeroPadding2D, Flatten, Activation, add
+from keras.layers.normalization import BatchNormalization
 from keras.models import Model
-from keras.optimizers import Adam
-from keras.preprocessing import image
-from keras.applications.resnet50 import conv_block, identity_block
-from keras.layers import (Activation, BatchNormalization, Convolution2D, Dense,
-                          Flatten, Input, MaxPooling2D, ZeroPadding2D, AveragePooling2D)
+from keras import initializers
+from keras.engine import Layer, InputSpec
 from keras import backend as K
-import numpy as np
-from keras import applications
-from keras.preprocessing.image import ImageDataGenerator
 from keras.utils import np_utils
+from keras.optimizers import *
+import numpy as np
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
 import dataset
 import argparse
 
 import time
 from datetime import timedelta
 
+
 def build_dataset(data_directory, img_width):
     X, y, tags = dataset.dataset(data_directory, int(img_width))
     nb_classes = len(tags)
 
     sample_count = len(y)
-    train_size = sample_count * 4 // 5
+    train_size = sample_count
     print("train size : {}".format(train_size))
-    X_train = X[:train_size]
-    y_train = y[:train_size]
-    Y_train = np_utils.to_categorical(y_train, nb_classes)
-    X_test  = X[train_size:]
-    y_test  = y[train_size:]
-    Y_test = np_utils.to_categorical(y_test, nb_classes)
-    return X_train, Y_train, X_test, Y_test, nb_classes
+    feature = X
+    label = np_utils.to_categorical(y, nb_classes)
+    return feature, label, nb_classes
 
-def build_model(SHAPE,nb_classes,bn_axis,seed=None):
+
+def identity_block(input_tensor, kernel_size, filters, stage, block):
+    """The identity block is the block that has no conv layer at shortcut.
+    # Arguments
+        input_tensor: input tensor
+        kernel_size: default 3, the kernel size of middle conv layer at main path
+        filters: list of integers, the filters of 3 conv layer at main path
+        stage: integer, current stage label, used for generating layer names
+        block: 'a','b'..., current block label, used for generating layer names
+    # Returns
+        Output tensor for the block.
+    """
+    filters1, filters2, filters3 = filters
+    if K.image_data_format() == 'channels_last':
+        bn_axis = 3
+    else:
+        bn_axis = 1
+    conv_name_base = 'res' + str(stage) + block + '_branch'
+    bn_name_base = 'bn' + str(stage) + block + '_branch'
+
+    x = Conv2D(filters1, (1, 1), name=conv_name_base + '2a')(input_tensor)
+    x = BatchNormalization(axis=bn_axis, name=bn_name_base + '2a')(x)
+    x = Activation('relu')(x)
+
+    x = Conv2D(filters2, kernel_size,
+               padding='same', name=conv_name_base + '2b')(x)
+    x = BatchNormalization(axis=bn_axis, name=bn_name_base + '2b')(x)
+    x = Activation('relu')(x)
+
+    x = Conv2D(filters3, (1, 1), name=conv_name_base + '2c')(x)
+    x = BatchNormalization(axis=bn_axis, name=bn_name_base + '2c')(x)
+
+    x = add([x, input_tensor])
+    x = Activation('relu')(x)
+    return x
+
+
+def conv_block(input_tensor, kernel_size, filters, stage, block, strides=(2, 2)):
+    """A block that has a conv layer at shortcut.
+    # Arguments
+        input_tensor: input tensor
+        kernel_size: default 3, the kernel size of middle conv layer at main path
+        filters: list of integers, the filters of 3 conv layer at main path
+        stage: integer, current stage label, used for generating layer names
+        block: 'a','b'..., current block label, used for generating layer names
+        strides: Strides for the first conv layer in the block.
+    # Returns
+        Output tensor for the block.
+    Note that from stage 3,
+    the first conv layer at main path is with strides=(2, 2)
+    And the shortcut should have strides=(2, 2) as well
+    """
+    filters1, filters2, filters3 = filters
+    if K.image_data_format() == 'channels_last':
+        bn_axis = 3
+    else:
+        bn_axis = 1
+    conv_name_base = 'res' + str(stage) + block + '_branch'
+    bn_name_base = 'bn' + str(stage) + block + '_branch'
+
+    x = Conv2D(filters1, (1, 1), strides=strides,
+               name=conv_name_base + '2a')(input_tensor)
+    x = BatchNormalization(axis=bn_axis, name=bn_name_base + '2a')(x)
+    x = Activation('relu')(x)
+
+    x = Conv2D(filters2, kernel_size, padding='same',
+               name=conv_name_base + '2b')(x)
+    x = BatchNormalization(axis=bn_axis, name=bn_name_base + '2b')(x)
+    x = Activation('relu')(x)
+
+    x = Conv2D(filters3, (1, 1), name=conv_name_base + '2c')(x)
+    x = BatchNormalization(axis=bn_axis, name=bn_name_base + '2c')(x)
+
+    shortcut = Conv2D(filters3, (1, 1), strides=strides,
+                      name=conv_name_base + '1')(input_tensor)
+    shortcut = BatchNormalization(
+        axis=bn_axis, name=bn_name_base + '1')(shortcut)
+
+    x = add([x, shortcut])
+    x = Activation('relu')(x)
+    return x
+
+
+def build_model(SHAPE, nb_classes, bn_axis, seed=None):
     # We can't use ResNet50 directly, as it might cause a negative dimension
     # error.
     if seed:
@@ -55,18 +133,16 @@ def build_model(SHAPE,nb_classes,bn_axis,seed=None):
     input_layer = Input(shape=SHAPE)
 
     x = ZeroPadding2D((3, 3))(input_layer)
-    x = Convolution2D(64, 7, 7, subsample=(2, 2), name='conv1')(x)
+    x = Conv2D(64, 7, 7, subsample=(2, 2), name='conv1')(x)
     x = BatchNormalization(axis=bn_axis, name='bn_conv1')(x)
     x = Activation('relu')(x)
-    x = MaxPooling2D((3, 3), strides=(2, 2))(x)
+    # x = MaxPooling2D((3, 3), strides=(2, 2))(x)
 
     x = conv_block(x, 3, [64, 64, 256], stage=2, block='a', strides=(1, 1))
     x = identity_block(x, 3, [64, 64, 256], stage=2, block='b')
     x = identity_block(x, 3, [64, 64, 256], stage=2, block='c')
-
-    #print(x)
-    #x = AveragePooling2D((7, 7), name='avg_pool')(x)
-
+    # print("x nya {}".format(x))
+    # x = AveragePooling2D((7, 7), name='avg_pool')(x)
 
     x = Flatten()(x)
     x = Dense(nb_classes, activation='softmax', name='fc10')(x)
@@ -75,6 +151,7 @@ def build_model(SHAPE,nb_classes,bn_axis,seed=None):
 
     return model
 
+
 def main():
     start_time = time.monotonic()
     parser = argparse.ArgumentParser(
@@ -82,45 +159,98 @@ def main():
     parser.add_argument('-i', '--input',
                         help='an input directory of dataset', required=True)
     parser.add_argument('-d', '--dimension',
-                        help='a image dimension', type=int, default=200)
+                        help='a image dimension', type=int, default=48)
     parser.add_argument('-c', '--channel',
                         help='a image channel', type=int, default=3)
     parser.add_argument('-e', '--epochs',
                         help='num of epochs', type=int, default=10)
     parser.add_argument('-b', '--batch_size',
                         help='num of batch_size', type=int, default=64)
-    parser.add_argument('-o', '--optimizer',
-                        help='choose the optimizer (rmsprop, adagrad, adadelta, adam, adamax, nadam)', default="adam")
+    # parser.add_argument('-o', '--optimizer',
+    #                     help='choose the optimizer (rmsprop, adagrad, adadelta, adam, adamax, nadam)', default="adam")
+    parser.add_argument('-o', '--output',
+                        help='a result file', type=str, default="hasilnya.txt")
     args = parser.parse_args()
     # dimensions of our images.
     img_width, img_height = args.dimension, args.dimension
     channel = args.channel
     epochs = args.epochs
     batch_size = args.batch_size
-    SHAPE = (img_width, img_height ,channel)
-    # print("SHAPE : {}".format(SHAPE))
+    SHAPE = (img_width, img_height, channel)
     bn_axis = 3 if K.image_dim_ordering() == 'tf' else 1
 
     data_directory = args.input
     period_name = data_directory.split('/')
 
-    print ("loading dataset")
-    X_train, Y_train, X_test, Y_test, nb_classes= build_dataset(data_directory, args.dimension)
+    print("loading dataset")
+    X_train, Y_train, nb_classes = build_dataset(
+        "{}/training/classes".format(data_directory), args.dimension)
+    X_test, Y_test, nb_classes = build_dataset(
+        "{}/testing/classes".format(data_directory), args.dimension)
     print("number of classes : {}".format(nb_classes))
 
-    model = build_model(SHAPE,nb_classes,bn_axis)
+    model = build_model(SHAPE, nb_classes, bn_axis)
 
-    model.compile(optimizer=args.optimizer, loss='categorical_crossentropy', metrics=['accuracy'])
+    model.compile(optimizer=Adam(lr=1.0e-4),
+                  loss='categorical_crossentropy', metrics=['accuracy'])
 
     # Fit the model
     model.fit(X_train, Y_train, batch_size=batch_size, epochs=epochs)
 
     # Save Model or creates a HDF5 file
-    model.save('{}epochs_{}period_{}dimension_resnet2_model.h5'.format(epochs,period_name[2],period_name[1]), overwrite=True)
-    #del model  # deletes the existing model
+    model.save('{}epochs_{}period_{}dimension_resnet2_model.h5'.format(
+        epochs, period_name[2], period_name[1]), overwrite=True)
+    # del model  # deletes the existing model
+    predicted = model.predict(X_test)
+    y_pred = np.argmax(predicted, axis=1)
+    Y_test = np.argmax(Y_test, axis=1)
+    cm = confusion_matrix(Y_test, y_pred)
+    report = classification_report(Y_test, y_pred)
+    tn = cm[0][0]
+    fn = cm[1][0]
+    tp = cm[1][1]
+    fp = cm[0][1]
+    if tp == 0:
+        tp = 1
+    if tn == 0:
+        tn = 1
+    if fp == 0:
+        fp = 1
+    if fn == 0:
+        fn = 1
+    TPR = float(tp)/(float(tp)+float(fn))
+    FPR = float(fp)/(float(fp)+float(tn))
+    accuracy = round((float(tp) + float(tn))/(float(tp) +
+                                              float(fp) + float(fn) + float(tn)), 3)
+    specitivity = round(float(tn)/(float(tn) + float(fp)), 3)
+    sensitivity = round(float(tp)/(float(tp) + float(fn)), 3)
+    mcc = round((float(tp)*float(tn) - float(fp)*float(fn))/math.sqrt(
+        (float(tp)+float(fp))
+        * (float(tp)+float(fn))
+        * (float(tn)+float(fp))
+        * (float(tn)+float(fn))
+    ), 3)
 
+    f_output = open(args.output, 'a')
+    f_output.write('=======\n')
+    f_output.write('{}epochs_{}period_{}dimension_resnet2\n'.format(
+        epochs, period_name[2], period_name[1]))
+    f_output.write('TN: {}\n'.format(tn))
+    f_output.write('FN: {}\n'.format(fn))
+    f_output.write('TP: {}\n'.format(tp))
+    f_output.write('FP: {}\n'.format(fp))
+    f_output.write('TPR: {}\n'.format(TPR))
+    f_output.write('FPR: {}\n'.format(FPR))
+    f_output.write('accuracy: {}\n'.format(accuracy))
+    f_output.write('specitivity: {}\n'.format(specitivity))
+    f_output.write("sensitivity : {}\n".format(sensitivity))
+    f_output.write("mcc : {}\n".format(mcc))
+    f_output.write("{}".format(report))
+    f_output.write('=======\n')
+    f_output.close()
     end_time = time.monotonic()
     print("Duration : {}".format(timedelta(seconds=end_time - start_time)))
+
 
 if __name__ == "__main__":
     main()
