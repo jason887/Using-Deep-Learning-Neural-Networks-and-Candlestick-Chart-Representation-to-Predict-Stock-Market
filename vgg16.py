2f89748994d9a1801110b431366121337a36560b
Rosdyana Kusuma <rosdyana.kusuma@gmail.com>
2018-06-10 21:52:30 +0800

    add func to count files vgg16

--- vgg16.py
+++ vgg16.py
@@ -15,19 +15,28 @@ from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
 from keras.layers import Dropout, Flatten, Dense
 from keras import applications
+import sys
+
+
+def countImage(input):
+    num_file = sum([len(files) for r, d, files in os.walk(input)])
+    num_dir = sum([len(d) for r, d, files in os.walk(input)])
+    print("num of files : {}\nnum of dir : {}".format(num_file, num_dir))
+    return int(num_file)
+
 
 # dimensions of our images.
 img_width, img_height = 200, 200
 
-period = 5
-
+period = 20
+datapath = sys.argv[1]
+epochs = int(sys.argv[1])
+batch_size = int(sys.argv[2])
 top_model_weights_path = 'bottleneck_fc_model_vgg16_{}.h5'.format(period)
-train_data_dir = 'dataset/{}/training'.format(period)
-validation_data_dir = 'dataset/{}/testing'.format(period)
-nb_train_samples = 4144 #1200 #1456 #6928 #4144
-nb_validation_samples = 272 #293 #272 #32 #256 #224
-epochs = 100
-batch_size = 16
+train_data_dir = '{}/train'.format(datapath)
+validation_data_dir = '{}/test'.format(datapath)
+nb_train_samples = countImage(train_data_dir)
+nb_validation_samples = countImage(validation_data_dir)
 
 
 def save_bottlebeck_features():
@@ -44,7 +53,8 @@ def save_bottlebeck_features():
         shuffle=False)
     bottleneck_features_train = model.predict_generator(
         generator, nb_train_samples // batch_size)
-    np.save('bottleneck_features_train_vgg16_{}'.format(period), bottleneck_features_train)
+    np.save('bottleneck_features_train_vgg16_{}'.format(
+        period), bottleneck_features_train)
 
     generator = datagen.flow_from_directory(
         validation_data_dir,
@@ -59,11 +69,13 @@ def save_bottlebeck_features():
 
 
 def train_top_model():
-    train_data = np.load('bottleneck_features_train_vgg16_{}.npy'.format(period))
+    train_data = np.load(
+        'bottleneck_features_train_vgg16_{}.npy'.format(period))
     train_labels = np.array(
         [0] * (nb_train_samples // 2) + [1] * (nb_train_samples // 2))
 
-    validation_data = np.load('bottleneck_features_validation_vgg16_{}.npy'.format(period))
+    validation_data = np.load(
+        'bottleneck_features_validation_vgg16_{}.npy'.format(period))
     validation_labels = np.array(
         [0] * (nb_validation_samples // 2) + [1] * (nb_validation_samples // 2))
 
@@ -90,7 +102,7 @@ def train_top_model():
     # print('Overall Test score: {}'.format(test_score[0]))
     # print('Overall Test accuracy: {}'.format(test_score[1]))
 
-    f_output = open("{}_{}.txt".format(top_model_weights_path,period),'a')
+    f_output = open("{}_{}.txt".format(top_model_weights_path, period), 'a')
     f_output.write('=======\n')
     f_output.write('Overall Train score: {}\n'.format(train_score[0]))
     f_output.write('Overall Train accuracy: {}\n'.format(train_score[1]))
