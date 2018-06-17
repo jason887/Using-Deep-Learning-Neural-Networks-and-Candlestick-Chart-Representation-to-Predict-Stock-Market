7fa06179e25adf0fe5727ebfa930c0f6fa691e0f
Rosdyana Kusuma <rosdyana.kusuma@gmail.com>
2018-06-17 17:32:15 +0800

    using PIL to check img mode

--- removezeroimage.py
+++ removezeroimage.py
@@ -1,6 +1,6 @@
 import os
 import sys
-import scipy.misc
+from PIL import Image
 
 pathdir = sys.argv[1]
 origindir = sys.argv[2]
@@ -9,11 +9,11 @@ countme = 0
 # dir walk to get all images file
 for root, dirs, files in os.walk("{}/{}".format(pathdir, origindir)):
     for file in files:
-        if file[0] == '0':
-            pathimg = "{}/{}".format(root, file)
-            img = scipy.misc.imread(pathimg)
-            height, width, chan = img.shape
-            if not chan == 3:
-                os.remove(pathimg)
-                countme += 1
+        pathimg = "{}/{}".format(root, file)
+        img = Image.open(pathimg)
+        xx = img.mode
+        if not xx == 'P':
+            os.remove(pathimg)
+            countme += 1
+
 print(countme)
